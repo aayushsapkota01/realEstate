4ec41bbf9cc1173c4252acbc2bf67e933dcb23ed
aayushsapkota01 <sapkotaaayush10@gmail.com>
2024-08-07 02:08:23 -0400

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        {/* Link to home page */}
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Aayush</span>
@@ -12,6 +13,7 @@ const Header = () => {
           </h1>
         </Link>
 
+        {/* Search form */}
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -20,6 +22,8 @@ const Header = () => {
           />
           <FaSearch className="text-slate-600" />
         </form>
+
+        {/* Navigation links */}
         <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
