4253b7f23a0297106d35e911d3afbf3154d21ab3
aayushsapkota01 <sapkotaaayush10@gmail.com>
2024-08-23 15:43:04 -0400

    complete update listing functionality

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -20,6 +21,7 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
