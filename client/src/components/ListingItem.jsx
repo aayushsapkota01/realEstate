6c9fa4aa0f60ac6f13315b7ec17c28264caab214
aayushsapkota01 <sapkotaaayush10@gmail.com>
2024-08-29 04:12:20 -0400

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -2,13 +2,41 @@ import { Link } from 'react-router-dom';
 import { MdLocationOn } from 'react-icons/md';
 import PropTypes from 'prop-types';
 
-export default function ListingItem({ listing }) {
+export default function ListingItem({
+  listing = {
+    _id: '',
+    imageUrls: [],
+    name: '',
+    address: '',
+    description: '',
+    offer: false,
+    discountPrice: 0,
+    regularPrice: 0,
+    type: '',
+    bedrooms: 0,
+    bathrooms: 1,
+  },
+}) {
+  const {
+    _id,
+    imageUrls,
+    name,
+    address,
+    description,
+    offer,
+    discountPrice,
+    regularPrice,
+    type,
+    bedrooms,
+    bathrooms,
+  } = listing;
+
   return (
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
-      <Link to={`/listing/${listing._id}`}>
+      <Link to={`/listing/${_id}`}>
         <img
           src={
-            listing.imageUrls[0] ||
+            imageUrls[0] ||
             'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
           }
           alt='listing cover'
@@ -16,34 +44,34 @@ export default function ListingItem({ listing }) {
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
           <p className='truncate text-lg font-semibold text-slate-700'>
-            {listing.name}
+            {name}
           </p>
           <div className='flex items-center gap-1'>
             <MdLocationOn className='h-4 w-4 text-green-700' />
             <p className='text-sm text-gray-600 truncate w-full'>
-              {listing.address}
+              {address}
             </p>
           </div>
           <p className='text-sm text-gray-600 line-clamp-2'>
-            {listing.description}
+            {description}
           </p>
           <p className='text-slate-500 mt-2 font-semibold '>
             $
-            {listing.offer
-              ? listing.discountPrice.toLocaleString('en-US')
-              : listing.regularPrice.toLocaleString('en-US')}
-            {listing.type === 'rent' && ' / month'}
+            {offer
+              ? discountPrice.toLocaleString('en-US')
+              : regularPrice.toLocaleString('en-US')}
+            {type === 'rent' && ' / month'}
           </p>
           <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} beds `
-                : `${listing.bedrooms} bed `}
+              {bedrooms > 1
+                ? `${bedrooms} beds `
+                : `${bedrooms} bed `}
             </div>
             <div className='font-bold text-xs'>
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} baths `
-                : `${listing.bathrooms} bath `}
+              {bathrooms > 1
+                ? `${bathrooms} baths `
+                : `${bathrooms} bath `}
             </div>
           </div>
         </div>
@@ -53,17 +81,17 @@ export default function ListingItem({ listing }) {
 }
 
 ListingItem.propTypes = {
-    listing: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      imageUrls: PropTypes.arrayOf(PropTypes.string),
-      name: PropTypes.string.isRequired,
-      address: PropTypes.string.isRequired,
-      description: PropTypes.string,
-      offer: PropTypes.bool,
-      discountPrice: PropTypes.number,
-      regularPrice: PropTypes.number.isRequired,
-      type: PropTypes.string.isRequired,
-      bedrooms: PropTypes.number.isRequired,
-      bathrooms: PropTypes.number.isRequired,
-    }).isRequired,
-  };
+  listing: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    imageUrls: PropTypes.arrayOf(PropTypes.string),
+    name: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    offer: PropTypes.bool,
+    discountPrice: PropTypes.number,
+    regularPrice: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    bedrooms: PropTypes.number.isRequired,
+    bathrooms: PropTypes.number,
+  }),
+};
